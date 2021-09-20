5e3e8597c6824b3c978d3d83bd38e975c66b447a
Fedir Katushonok <mtg.in.ua@gmail.com>
2021-09-20 21:47:08 +0300

    initial

---
+++ RSB_Implementation/EncryptionRound.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSB_Implementation
+{
+    public class EncryptionRound
+    {
+
+    }
+}
---
+++ RSB_Implementation/Program.cs
@@ -0,0 +1,18 @@
+﻿using RSB_Implementation;
+using System.Text;
+
+string rawText = "Hello world!";
+const int blockLength = 256;
+
+
+var rawTextBytes = Encoding.UTF8.GetBytes(rawText).ToList();
+
+var roundKey = KeyGen.CreateRandomRoundKey();
+
+var byteBlocksCount = blockLength / 8;
+
+int remnantBytesCount = byteBlocksCount - rawTextBytes.Count % byteBlocksCount;
+
+rawTextBytes.AddRange(Enumerable.Repeat<byte>(0, remnantBytesCount));
+
+Console.ReadLine();
