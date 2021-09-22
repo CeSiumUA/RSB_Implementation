f30183dfc70fef01be7e5b62235fa958fd8f0620
Fedir Katushonok <mtg.in.ua@gmail.com>
2021-09-22 07:41:07 +0300

    added key splitter

--- RSB_Implementation/Program.cs
+++ RSB_Implementation/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 string rawText = "Hello world!";
 const int blockLength = 64;
 const int encryptionSteps = 2;
+const int roundKeyLength = 32;
 const int keyLength = 64;
 
 int encryptionRounds = encryptionSteps * (int)Math.Pow(2, blockLength / 64);
@@ -23,7 +24,7 @@ rawTextBytes.AddRange(Enumerable.Repeat<byte>(0, remnantBytesCount));
 
 for(int iteration = 0; iteration < encryptionSteps; iteration++)
 {
-
+    var splittedKey = commonKey.Skip(iteration * roundKeyLength / 8).Take(roundKeyLength / 8).ToArray();
 }
 
 Console.ReadLine();
