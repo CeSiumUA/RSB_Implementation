b6768254747c64e2e8001c84519fb31d6a3c2a00
Fedir Katushonok <mtg.in.ua@gmail.com>
2021-10-25 23:58:50 +0300

    Added default value

--- RSB_GUI/Settings.cs
+++ RSB_GUI/Settings.cs
@@ -10,7 +10,7 @@ namespace RSB_GUI
     public class Settings
     {
         public int BlockPower { get; set; }
-        public int LogBlockLength { get; set; }
+        public int LogBlockLength { get; set; } = 8;
         public int ShiftValue { get; set; }
         public string InputFilePath { get; set; } = string.Empty;
         public string OutputFilePath { get; set; } = string.Empty;
