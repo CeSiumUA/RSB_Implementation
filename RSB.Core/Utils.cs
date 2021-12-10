d05a1b06ba619e7f93b4dca6d429d445a3d5b968
Fedir Katushonok <mtg.in.ua@gmail.com>
2021-12-10 21:05:42 +0200

    added core project

---
+++ RSB.Core/Utils.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+
+namespace RSB.Core
+{
+    public class Utils
+    {
+        public static byte[] FillRemnantBytes(byte[] data, int blockSizeBytes)
+        {
+            var remnantBytes = data.Length % blockSizeBytes;
+            var bytesToAdd = remnantBytes == 0 ? remnantBytes : blockSizeBytes - remnantBytes;
+            byte[] filledArray = new byte[data.Length + bytesToAdd];
+            Array.Copy(data, 0, filledArray, 0, data.Length);
+            return filledArray;
+        }
+        public static byte[] GenerateBytes(int count)
+        {
+            byte[] bytes = new byte[count];
+            using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(bytes);
+            }
+            return bytes;
+        }
+    }
+}
--- SlideCoder/Program.cs
+++ SlideCoder/Program.cs
@@ -1,5 +1,33 @@
-﻿const int KeyLength = 128;
-const int BlockLength = 192;
-int rounds = KeyLength / 32;
+﻿using RSB.Core;
+using System.Collections;
+using System.Security.Cryptography;
 
-var bitsGreyCount = 32;
+const int KeyLength = 128;
+const int BlockLength = 192;
+int blockSizeBytes = BlockLength / 8;
+int rounds = KeyLength / 32;
+int graySchemeBytes = 2;
+byte[] rawData = File.ReadAllBytes(@"C:\Users\mtgin\Downloads\DALF.doc");
+
+var data = Utils.FillRemnantBytes(rawData, blockSizeBytes);
+
+byte[] keyBytes = Utils.GenerateBytes(KeyLength / 8);
+
+for(int i = 0; i < data.Length / blockSizeBytes; i++)
+{
+    var bytesToProcess = new byte[blockSizeBytes];
+    Array.Copy(data, 0, bytesToProcess, i * blockSizeBytes, blockSizeBytes);
+    for (int r = 0; r < rounds; r++)
+    {
+        var roundKeyBytes = new byte[4];
+        Array.Copy(keyBytes, r * 4, roundKeyBytes, 0, 4);
+        var runs = roundKeyBytes.Length / graySchemeBytes;
+        for(int run = 0; run < runs; run++)
+        {
+            var runKeyBytes = new byte[graySchemeBytes];
+            Array.Copy(roundKeyBytes, run * graySchemeBytes, runKeyBytes, 0, runKeyBytes.Length);
+        }
+    }
+}
+
+Console.ReadLine();
