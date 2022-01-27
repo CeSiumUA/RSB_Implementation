68908a42c81047f2f0cf7903e02eb2a4089756e6
Fedir Katushonok <fedir.katushonok@gmail.com>
2022-01-27 21:12:40 +0200

    Moved button

---
+++ RSB_GUI/Utils/Testing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSB_GUI.Utils
+{
+    [Serializable]
+    internal class Testing
+    {
+        public int Variant { get; set; }
+        public int Rounds { get; set; }
+        public int BlockLength { get; set; }
+        public double Entropy { get; set; }
+        public double TimeToComplete { get; set; }
+    }
+}
